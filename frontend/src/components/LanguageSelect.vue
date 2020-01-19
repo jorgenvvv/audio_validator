065b321b0e1421eb45f6458d9cb6c042f50bd80e
jorgenvvv <jorgenvvv@gmail.com>
2020-01-19 15:28:20 +0200

    Fix bugs related to JWT token

--- frontend/src/components/LanguageSelect.vue
+++ frontend/src/components/LanguageSelect.vue
@@ -1,12 +1,24 @@
 <template>
   <v-container>
     <v-list>
-      <v-list-item v-for="lang in availableLanguages" :key="lang.code" @click="chooseLanguage(lang.code)">
+      <v-list-item
+        v-for="lang in availableLanguages"
+        :key="lang.code"
+        @click="chooseLanguage(lang.code)"
+      >
         <v-list-item-content>
-          <v-list-item-title>{{ lang.name }} ({{ lang.validated }} / {{ lang.total }})</v-list-item-title>
+          <v-list-item-title
+            >{{ lang.name }} ({{ lang.validated }} /
+            {{ lang.total }})</v-list-item-title
+          >
           <v-list-item-subtitle>
-            <v-progress-linear :value="validatedPercentage(lang.total, lang.validated)" height="25">
-              <strong>{{ validatedPercentage(lang.total, lang.validated) }}%</strong>
+            <v-progress-linear
+              :value="validatedPercentage(lang.total, lang.validated)"
+              height="25"
+            >
+              <strong
+                >{{ validatedPercentage(lang.total, lang.validated) }}%</strong
+              >
             </v-progress-linear>
           </v-list-item-subtitle>
         </v-list-item-content>
@@ -22,23 +34,22 @@ export default {
     return {
       availableLanguages: [],
       selectedLanguage: null
-    }
+    };
   },
 
   created() {
-    axios.get(process.env.VUE_APP_API_URL + '/languages/all')
-      .then(response => {
-        this.availableLanguages = response.data;
-      });
+    axios.get(process.env.VUE_APP_API_URL + '/languages/all').then(response => {
+      this.availableLanguages = response.data;
+    });
   },
 
   methods: {
     chooseLanguage(langCode) {
-      this.$router.push({ name: 'audiovalidator', params: { lang: langCode } })
+      this.$router.push({ name: 'audiovalidator', params: { lang: langCode } });
     },
 
     validatedPercentage(total, validated) {
-      return (validated / total * 100).toFixed(1);
+      return ((validated / total) * 100).toFixed(1);
     }
   }
 };
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import VueRouter from 'vue-router'
-import VueAxios from 'vue-axios'
-import VueAuthenticate from 'vue-authenticate'
-import axios from 'axios'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import VueAuthenticate from 'vue-authenticate';
+import axios from 'axios';
 
 import LanguageSelect from './components/LanguageSelect';
 import AudioValidator from './components/AudioValidator';
 import Home from './components/Home';
 
-Vue.config.productionTip = false
+import { store } from './store.js';
 
+Vue.config.productionTip = false;
 
-Vue.use(VueRouter)
-Vue.use(VueAxios, axios)
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
 Vue.use(VueAuthenticate, {
   baseUrl: process.env.VUE_APP_API_URL,
   providers: {
@@ -29,27 +30,34 @@ Vue.use(VueAuthenticate, {
         config.headers['Authorization'] = [
           this.options.tokenType,
           this.getToken()
-        ].join(' ')
+        ].join(' ');
       } else {
-        delete config.headers['Authorization']
+        delete config.headers['Authorization'];
       }
 
-      return config
-    })
+      return config;
+    });
 
     this.$http.interceptors.response.use(response => {
-      this.setToken(response)
-      return response
-    })
+      this.setToken(response);
+      return response;
+    });
   }
-})
+});
 
-Vue.prototype.$API_URL = process.env.VUE_APP_API_URL
+Vue.prototype.$API_URL = process.env.VUE_APP_API_URL;
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/auth/callback', name: 'auth' },
-  { path: '/languages', name: 'languages', component: LanguageSelect },
+  {
+    path: '/languages',
+    name: 'languages',
+    component: LanguageSelect,
+    meta: {
+      requiresAuth: true
+    }
+  },
   {
     path: '/languages/:lang/validate',
     name: 'audiovalidator',
@@ -58,24 +66,30 @@ const routes = [
       requiresAuth: true
     }
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
 // Make sure that the next function is called exactly once in any given pass through the navigation guard. It can appear more than once, but only if the logical paths have no overlap, otherwise the hook will never be resolved or produce errors.
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!router.app.$auth.isAuthenticated()) next('/')
-    else next();
+    if (!router.app.$auth.isAuthenticated()) {
+      store.setAuthenticated(false);
+      next('/');
+    } else {
+      next();
+    }
   } else {
-    next()
+    if (!router.app.$auth.isAuthenticated()) store.setAuthenticated(false);
+
+    next();
   }
-})
+});
 
 new Vue({
   vuetify,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
